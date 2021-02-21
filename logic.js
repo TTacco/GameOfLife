fbdfdd0bce848fdedc704b64d619e721a40b48f5
TTacco <joventadem@gmail.com>
2021-02-21 17:44:28 +0800

    added main html, css and js files

---
+++ logic.js
@@ -0,0 +1,10 @@
+
+var body = document.getElementById("gridbody");
+
+for(i = 0; i < 625; i++){
+    var body = document.getElementById("gridbody");
+    var block = document.createElement("null");
+    body.appendChild(block);
+}
+
+
