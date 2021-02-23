b1f2cb699a482864b9823bcff0f53b1e8988da1c
TTacco <joventadem@gmail.com>
2021-02-23 13:37:13 +0800

    early cell interactability

--- logic.js
+++ logic.js
@@ -1,10 +1,19 @@
 
-var body = document.getElementById("gridbody");
 
-for(i = 0; i < 625; i++){
+function drawBoard(){
     var body = document.getElementById("gridbody");
-    var block = document.createElement("null");
-    body.appendChild(block);
+
+    for(i = 0; i < 625; i++){
+        var body = document.getElementById("gridbody");
+        var cell = document.createElement("cell");
+        cell.addEventListener("click", clickMe);
+        body.appendChild(cell);
+    }
 }
 
+function clickMe(event){
+    console.log(event);
+    event.target.style.backgroundColor = "red";
+}
 
+drawBoard();
